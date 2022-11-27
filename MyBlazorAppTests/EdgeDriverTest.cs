0585737c7c89f4487d76fb9b79ec766e1582fad3
yehudak <yehudakre@iplan.gov.il>
2022-11-27 17:14:07 +0200

    add test project

---
+++ MyBlazorAppTests/EdgeDriverTest.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace MyBlazorAppTests
+{
+    [TestClass]
+    public class EdgeDriverTest
+    {
+        private static IWebDriver _driver;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--ignore-certificate-errors", "--headless");
+
+            bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            if (isWindows)
+            {
+                using WindowsIdentity identity = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+                {
+                    options.AddArgument("--no-sandbox");
+                }
+            }
+
+            _driver = new ChromeDriver(options);
+        }
+
+        [TestMethod]
+        public void VerifyPageTitle()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:5058");
+            Assert.AreEqual("Counter", _driver.Title);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _driver.Quit();
+        }
+    }
+}
